97f555acbda4ff066bd11914582e2121e571bd95
Thomas Murphy <thomasmurphy1991@gmail.com>
2021-04-07 19:06:33 +0100

    chore(wip)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageHeaderComponent } from './page-header/page-header.component';
+import { PageBodyComponent } from './page-body/page-body.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageHeaderComponent,
+    PageBodyComponent
   ],
   imports: [
     BrowserModule,
